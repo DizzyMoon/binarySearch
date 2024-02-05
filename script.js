6bccc62d6a855871d8240a06cdcb0bdd0928f313
DizzyMoon <mikkel.simons1@gmail.com>
2024-02-05 03:10:35 +0100

    added ui

--- script.js
+++ script.js
@@ -3,13 +3,39 @@
 window.addEventListener("DOMContentLoaded", start)
 
 //Global variables
-let globalList = [1, 3, 4, 7, 8, 13, 19]
-
+let globalList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    , 11, 12, 13, 14, 15, 16, 17, 18, 19,
+     21, 22, 23, 24, 25, 26, 27, 28, 29,
+      30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+       40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+        51, 52, 53, 54, 55, 56, 57, 58, 59,
+         60, 61, 62, 63, 64, 65, 66, 67, 68, 69,
+          70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
+           80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
+            90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
 function start() {
     console.log("Javascript is running")
-    console.log("" + binarySearch(globalList, 19));
 }
 
+function buttonClicked(event){
+    
+    event.preventDefault();
+
+    const answerContainer = document.querySelector("#answer-container")
+    const number = parseInt(document.querySelector("#number-input").value, 10);
+    
+    let answer = binarySearch(globalList, number);
+
+    console.log("" + answer);
+
+    let answerElement = document.querySelector(".answer");
+
+    if (answer === -1){
+        answerElement.innerHTML = "The given number does not exist in the list";
+    } else {
+        answerElement.innerHTML = `The given number is at index: ${answer}`;
+    }
+}
 
 function binarySearch(listIn, number, offset = 0) {
     let maxIndex = listIn.length;
@@ -31,5 +57,5 @@ function binarySearch(listIn, number, offset = 0) {
         return binarySearch(newList, number, median + offset + 1);
     }
 
-    return -1
+    return -1;
 }
